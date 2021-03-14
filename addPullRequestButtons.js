f84fcb2e84c8543408967d89066eb3d1eb589aca
Diogo Araujo <diogo_araujo9@hotmail.com>
2021-03-13 21:33:16 -0300

    adicionar botões ao carregar página de pull requests

---
+++ addPullRequestButtons.js
@@ -0,0 +1,137 @@
+(() => {
+    let prButton = document.getElementById("__bolt-create-pr");
+    let buttonsContainer = prButton && prButton.parentNode;
+
+    let alreadyAdded = document.getElementById("__bolt-create-pr-beta");
+
+    if (!alreadyAdded && prButton && buttonsContainer) {
+        createPrButtons(prButton, buttonsContainer);
+        createHotfixInput(prButton.href, buttonsContainer);
+        listenEventsToCreateMessageButtons();
+    }    
+})();
+
+function listenEventsToCreateMessageButtons() {
+    try {
+        let messageCard = document.getElementsByClassName("bolt-messagecard");
+
+        if (messageCard && messageCard.length) {
+            createMessageButtons();
+        }
+        else {
+            let pageContent = document.getElementsByClassName("page-content")[0];
+            MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    
+            var observer = new MutationObserver(function(mutations, observer) {
+                createMessageButtons();
+            });
+    
+            observer.observe(pageContent, {
+                subtree: true,
+                attributes: true,
+                childList: true
+            });
+        }    
+    }
+    catch(err) {
+        console.error(err);
+    }      
+};
+
+function createMessageButtons() {
+    let messageCard = document.getElementsByClassName("bolt-messagecard");
+    let alreadyAddedButton = document.getElementById("hotfixButton");
+
+    if (!alreadyAddedButton && messageCard && messageCard.length) {
+        let buttons = document.getElementsByClassName("bolt-link-button");
+        let buttonsContainer = document.getElementsByClassName("bolt-messagebar-buttons");
+
+        if (buttons && buttons.length && buttonsContainer && buttonsContainer.length) {
+            let hotfixButton = document.createElement("button");
+            hotfixButton.id = "hotfixButton";
+            hotfixButton.className = "bolt-header-command-item-button bolt-button enabled bolt-focus-treatment";
+            hotfixButton.innerHTML = "Create Hotfix";
+
+            let cardButton = buttons[buttons.length - 1];
+            let originalHref = cardButton.href.split("targetRef=master")[0];
+
+            hotfixButton.addEventListener('click', function (e) {
+                openInNewTab(originalHref + "&targetRef=stable");
+                openInNewTab(originalHref + "&targetRef=beta");
+                openInNewTab(originalHref + "&targetRef=master");
+                e.stopPropagation();
+            });
+
+            buttonsContainer[0].prepend(hotfixButton);
+        }
+    }
+};
+
+function createPrButtons(originalPrButton, container) {
+    try {
+        let betaPrButton = originalPrButton.cloneNode(true);
+
+        betaPrButton.href = betaPrButton.href + "?sourceRef=beta&targetRef=stable";
+        betaPrButton.id = "__bolt-create-pr-beta";
+        betaPrButton.children[0].innerText = "Beta > Stable";        
+        
+        container.prepend(betaPrButton);
+    
+        let masterPrButton = originalPrButton.cloneNode(true);
+        masterPrButton.href = masterPrButton.href + "?sourceRef=master&targetRef=beta";
+        masterPrButton.id = "__bolt-create-pr-master";
+        masterPrButton.children[0].innerText = "Master > Beta";
+        
+        container.prepend(masterPrButton);
+    }
+    catch(err) {
+        console.error(err);
+    }    
+};
+
+function createHotfixInput(originalHref, container) {
+    try {
+        let searchInput = document.getElementById("l1-search-input");
+
+        if (searchInput) {
+            let hotfixInput = searchInput.cloneNode(true);
+            hotfixInput.setAttribute("style", `align-self: stretch;
+                                                background: transparent;
+                                                overflow: hidden;
+                                                border-color: #A19F9D;
+                                                border: 1px solid;
+                                                width: 250px;
+                                                color: var(--nav-header-text-primary-color,rgba(0, 0, 0, .9));
+                                                border-radius: 2px;
+                                                padding-left: 10px;`);
+
+            hotfixInput.setAttribute("placeholder", "Nome da branch e aperte enter");
+
+            container.prepend(hotfixInput);
+
+            hotfixInput.addEventListener('keypress', function (e) {
+                if (e.key === 'Enter') {
+                    openInNewTab(originalHref + "?sourceRef=" + hotfixInput.value + "&targetRef=stable");
+                    openInNewTab(originalHref + "?sourceRef=" + hotfixInput.value + "&targetRef=beta");
+                    openInNewTab(originalHref + "?sourceRef=" + hotfixInput.value + "&targetRef=master");
+                }
+            });
+
+            let hotfixLabel = document.createElement("span");
+            hotfixLabel.innerHTML = "Hotfix: ";
+            hotfixLabel.className = "bolt-breadcrumb-item-text";
+
+            container.prepend(hotfixLabel);
+        }
+    }
+    catch(err) {
+        console.error(err);
+    }    
+};
+
+function openInNewTab(href) {
+    Object.assign(document.createElement('a'), {
+        target: '_blank',
+        href: href,
+    }).click();
+};
--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file: "fillTitle.js"});
 });
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    if (details.url && details.url.includes("wevo.visualstudio.com")) {
+        let paths = details.url.split("/");
+        
+        if (paths && paths.length && (!paths[paths.length - 1] || paths[paths.length - 1].startsWith("pullrequests"))) {
+            chrome.tabs.executeScript(details.tabId, {file: "addPullRequestButtons.js"});
+        }
+    }
+}, {
+    url: [{
+        hostContains: '.visualstudio.'
+    }]
+});
